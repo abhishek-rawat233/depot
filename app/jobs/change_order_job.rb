f1838f9a5f907bcf52a1e368809c8b7579992848
abhishek-rawat233 <abhishek.rawat@vinsol.com>
2019-11-09 15:58:09 +0530

    Emails_and_Payments

---
+++ app/jobs/change_order_job.rb
@@ -0,0 +1,8 @@
+class ChangeOrderJob < ApplicationJob
+  queue_as :default
+
+  def perform(order, pay_type_params)
+    # Do something later
+    order.charge!(pay_type_params)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  #for doing development with multiple machines
+  config.action_cable.disable_request_forgery_protection = true
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
