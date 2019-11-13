e3ccb4a51fe0d850cc1688e3a4693cd5c8d249cf
abhishek-rawat233 <abhishek.rawat@vinsol.com>
2019-11-13 14:56:36 +0530

    internationalization -partial

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,16 @@ Rails.application.routes.draw do
 
   resources :wsers
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  # get 'store/index'
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
+  # get 'store/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
