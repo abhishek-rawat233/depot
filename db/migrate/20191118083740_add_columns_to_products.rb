96d298192d5e8509be76f93e5b0face922624597
Himanshu <himanshumishra3@gmail.com>
2019-11-25 11:10:47 +0530

    migration review changes

--- db/migrate/20191118083740_add_columns_to_products.rb
+++ db/migrate/20191118083740_add_columns_to_products.rb
@@ -1,5 +1,6 @@
 class AddColumnsToProducts < ActiveRecord::Migration[6.0]
   def change
+    #ToDo: Separate all points in separate migrations
     add_column :products, :enabled, :boolean, default: false
     add_column :products, :discount_price, :decimal
     add_column :products, :permalink, :string
