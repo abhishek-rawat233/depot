da4dc1d2e92906c10ef7c59cb4044f024a59ebdc
abhishek-rawat233 <abhishek.rawat@vinsol.com>
2019-12-11 12:40:25 +0530

    exercise complete

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,7 @@
-class CategoryValidator < ActiveModel::Validator
-  def validate(record)
-    is_sub_category = Category.find(record.parent_id).parent_id
-    return unless is_sub_category
-    record.errors[:parent_id] << "is invalid. Sub-categories can't have sub_categories" if is_sub_category
-  end
-end
-
 class Category < ApplicationRecord
+  #exercise
+  has_many :sub_categories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
+  belongs_to :parent, class_name: "Category", optional: true
 
   has_many :products, dependent: :restrict_with_error
   has_many :products_of_sub_categories, through: :sub_categories, source: :products
@@ -14,15 +9,18 @@ class Category < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true, if: :name?, unless: :parent_id?
   validates_uniqueness_of :name, scope: :parent_id, if: :name?
-  validates_with CategoryValidator, if: :parent_id?
 
   before_destroy :sub_categories_has_products
-
-  has_many :sub_categories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
-  belongs_to :parent, class_name: "Category", optional: true
+  before_save :parent_cannot_be_subcategory, if: :parent_id?
 
   private
+    def parent_cannot_be_subcategory
+      return unless parent.parent.present?
+      errors[:parent_id] << "is invalid. Sub-categories can't have sub_categories"
+      throw :abort
+    end
+
     def sub_categories_has_products?
-      return products_of_sub_categories.present? ? false : true
+      throw :abort unless products_of_sub_categories.present?
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,3 @@
-# class UrlValidator < ActiveModel::EachValidator
-#   def validate_each(record, attribute, value)
-#    record.errors[attribute] << "is not valid" unless value =~ /regex/
-#   end
-# end
-
 class PriceValidator < ActiveModel::Validator
   def validate(record)
     return if record.price.nil?
@@ -12,18 +6,16 @@ class PriceValidator < ActiveModel::Validator
 end
 
 class Product < ApplicationRecord
-  has_many :line_items, dependent: :restrict_with_error #, before_remove: :ensure_not_referenced_by_any_line_item
+  has_many :line_items, dependent: :restrict_with_error
   has_many :orders, through: :line_items
   has_many :carts, through: :line_items
-  # before_destroy :ensure_not_referenced_by_any_line_item
+
   after_initialize :default_values
   belongs_to :category, counter_cache: :products_count
-  # after_create :category_counter
+
   after_update :category_counter
-  # belongs_to :sub_category
-  # frozen_string_literal: true
+
   validates :description, :image_url, presence: true
-  # validates :title, :description, :image_url, presence: true #to satify empty title callback this has been commented out
   validates :title, length: {minimum: 10}
   validates :description, length: { minimum: 5, maximum: 10, message: 'should be in between 5-10 characters'}
   validates :title, uniqueness: true
@@ -32,15 +24,10 @@ class Product < ApplicationRecord
     %r{\.(gif|jp(|e)g|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }, allow_blank: true
   validates :price, numericality: { greater_than_or_equal_to: :discount_price }
   validates_with PriceValidator
-
-  validates :permalink, format: { without:  /[^a-z0-9\-]+/i,
-    message: 'no space and special characters allowed'}
-    validates :permalink, format: { with: /(?:\w+\-){2,}\w+/,
-    message: 'enter atleast 3 words separated with hypen(-)' }
-    validates :permalink, uniqueness: true
 
   private
   def default_values
